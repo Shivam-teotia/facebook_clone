037ce7ab37a6a7fcf7976343863601761954807d
Shivam-Teotia <shivamteotia200@gamil.com>
2024-02-11 20:48:58 +0530

    section 6

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>App component</div>
-  <router-view></router-view>
+  <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+    <Nav />
+
+    <div class="flex overflow-y-hidden flex-1">
+      <Sidebar />
+
+      <div class="overflow-x-hidden w-2/3">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import Nav from "./Nav.vue";
+import Sidebar from "./Sidebar.vue";
 </script>
+
+<style scoped>
+</style>
